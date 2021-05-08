4156eaef62dcfa900af721bb7711b8e30d9a26b3
Skuuully <skuully@gmail.com>
2021-05-08 13:50:01 +0100

    Attempt with local site build

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Gravity game",
+        "excerpt":"A university module required the creation of a game that looks at and implements an advanced physics topic, and an advanced AI topic. For this I created the following available for download. For the physics topic I looked at gravity such that an object being an attractee could be attracted...","categories": [],
+        "tags": [],
+        "url": "/gravity/",
+        "teaser": null
+      },{
+        "title": "CVRS: Covid visualisation",
+        "excerpt":"For my independent studies dissertation I decided to look at creating a virtual reality application that depicts the transmission of COVID-19 in a university setting. The application allows for the transmission of COVID-19 between AI agents, and visualises this by using a border outline. The work builds upon that of...","categories": [],
+        "tags": [],
+        "url": "/cvrs/",
+        "teaser": null
+      }]
